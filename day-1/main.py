565bd3a353a816ded2bed905befbcb6cc2fe5cbd
christian <nwolle14@gmail.com>
2025-12-16 14:11:29 +0000

    day 1

---
+++ day-1/main.py
@@ -0,0 +1,11 @@
+
+print("WELCOME TO THE BAND NAME GENERATOR! ")
+
+# poser la question à l'utilisateur
+
+city = input("Quelle est le nom de ta ville? ")
+animal = input("Quel est le nom de ton animal? ")
+
+result = f"The brand name is {city} {animal}"
+
+print(result)
